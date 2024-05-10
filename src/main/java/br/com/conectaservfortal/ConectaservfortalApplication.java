2187efa0d3b12f4639e4102e68907369a1fe4d83
Anderson Freitas <andersonfreitas21@gmail.com>
2024-05-10 10:22:06 -0300

    first commit

---
+++ src/main/java/br/com/conectaservfortal/ConectaservfortalApplication.java
@@ -0,0 +1,13 @@
+package br.com.conectaservfortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaservfortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaservfortalApplication.class, args);
+	}
+
+}
